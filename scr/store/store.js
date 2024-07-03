90bf73e13ee77698f10e10b32606174cae057094
AZIMBEK SAGYNBAEV <sagynbaev6@gmail.com>
2024-07-04 00:43:28 +0600

    Update store.js

--- scr/store/store.js
+++ scr/store/store.js
@@ -2,5 +2,6 @@ import { createStore } from 'redux';
 import { cartReducer } from "./cartReducer";
 
 //Could you please provide pseudo code of how you store auth parameters in store?
+// e.g. Access Token and Refresh Token, UserId, FirstName and LastName
 
 export const store = createStore(cartReducer)
