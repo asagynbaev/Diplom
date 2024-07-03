751879420b16ffa369dc255ff2b694728c64eedc
AZIMBEK SAGYNBAEV <sagynbaev6@gmail.com>
2024-07-04 00:42:40 +0600

    Update store.js

--- scr/store/store.js
+++ scr/store/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
 import { cartReducer } from "./cartReducer";
 
-
+//Could you please provide pseudo code of how you store auth parameters in store?
 
 export const store = createStore(cartReducer)
